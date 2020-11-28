ba661739cf0ead9a0c0c24518d500e52fe58fc9e
wtong2017 <wtong@connect.ust.hk>
2020-11-28 16:08:57 +0800

    fix going back to previous location and add linter

--- src/index.js
+++ src/index.js
@@ -1,114 +1,127 @@
-const pdfjsLib = require('pdfjs-dist/webpack.js');
-import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer';
+import * as pdfjsViewer from "pdfjs-dist/web/pdf_viewer";
 
-import "pdfjs-dist/web/pdf_viewer.css"
+import "pdfjs-dist/web/pdf_viewer.css";
+const pdfjsLib = require("pdfjs-dist/webpack.js");
 
-var myState = {
-    pdf: null,
-    currentPage: 1,
-    zoom: 1
-}
+const myState = {
+  pdf: null,
+  currentPage: 1,
+  zoom: 1,
+};
 
-let lastPosition = null
+let lastPosition = null;
+let lastPositionOffset = null;
 
-var SEARCH_FOR = ""
+const SEARCH_FOR = "";
 
-// var CMAP_URL = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.5.207/cmaps/";
-// var CMAP_PACKED = true;
+// let CMAP_URL = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.5.207/cmaps/";
+// let CMAP_PACKED = true;
 
-var inputElement = document.getElementById("inputElement")
+const inputElement = document.getElementById("inputElement");
 if (inputElement) {
-    // load with input file
-    inputElement.onchange = function (event) {
+  // load with input file
+  inputElement.onchange = function (event) {
+    const file = event.target.files[0];
 
-        var file = event.target.files[0];
+    // Step 2: Read the file using file reader
+    const fileReader = new FileReader();
 
-        //Step 2: Read the file using file reader
-        var fileReader = new FileReader();
+    fileReader.onload = function () {
+      // Step 4:turn array buffer into typed array
+      const typedarray = new Uint8Array(this.result);
 
-        fileReader.onload = function () {
-
-            //Step 4:turn array buffer into typed array
-            var typedarray = new Uint8Array(this.result);
-
-            //Step 5:PDFJS should be able to read this
-            var loadingTask = pdfjsLib.getDocument({
-                data: typedarray,
-                // cMapUrl: CMAP_URL,
-                // cMapPacked: CMAP_PACKED
-            });
-            loadingTask.promise.then(function (pdf) {
-                // you can now use *pdf* here
-                myState.pdf = pdf;
-                pdfViewer.setDocument(myState.pdf);
-                pdfLinkService.setDocument(myState.pdf, null);
-                render();
-            });
-        };
-        //Step 3:Read the file as ArrayBuffer
-        fileReader.readAsArrayBuffer(file);
-    }
-} else {
-    // load with link query
-    let queryString = document.location.search.substring(1)
-    const urlParams = new URLSearchParams(queryString);
-    var loadingTask = pdfjsLib.getDocument({
-        url: urlParams.get("file"),
+      // Step 5:PDFJS should be able to read this
+      const loadingTask = pdfjsLib.getDocument({
+        data: typedarray,
         // cMapUrl: CMAP_URL,
         // cMapPacked: CMAP_PACKED
-    });
-    loadingTask.promise.then(function (pdf) {
+      });
+      loadingTask.promise.then(function (pdf) {
         // you can now use *pdf* here
         myState.pdf = pdf;
         pdfViewer.setDocument(myState.pdf);
         pdfLinkService.setDocument(myState.pdf, null);
         render();
-    });
+      });
+    };
+    // Step 3:Read the file as ArrayBuffer
+    fileReader.readAsArrayBuffer(file);
+  };
+} else {
+  // load with link query
+  const queryString = document.location.search.substring(1);
+  const urlParams = new URLSearchParams(queryString);
+  const loadingTask = pdfjsLib.getDocument({
+    url: urlParams.get("file"),
+    // cMapUrl: CMAP_URL,
+    // cMapPacked: CMAP_PACKED
+  });
+  loadingTask.promise.then(function (pdf) {
+    // you can now use *pdf* here
+    myState.pdf = pdf;
+    pdfViewer.setDocument(myState.pdf);
+    pdfLinkService.setDocument(myState.pdf, null);
+    render();
+  });
 }
 
-var container = document.getElementById("viewerContainer");
-var eventBus = new pdfjsViewer.EventBus();
-var pdfLinkService = new pdfjsViewer.PDFLinkService({
-    eventBus: eventBus,
-    externalLinkTarget: 2 // new tab
+const container = document.getElementById("viewerContainer");
+const viewer = document.getElementById("viewer");
+const eventBus = new pdfjsViewer.EventBus();
+const pdfLinkService = new pdfjsViewer.PDFLinkService({
+  eventBus: eventBus,
+  externalLinkTarget: 2, // new tab
 });
-var pdfFindController = new pdfjsViewer.PDFFindController({
-    eventBus: eventBus,
-    linkService: pdfLinkService,
+const pdfFindController = new pdfjsViewer.PDFFindController({
+  eventBus: eventBus,
+  linkService: pdfLinkService,
 });
 
-var pdfViewer = new pdfjsViewer.PDFViewer({
-    container: container,
-    eventBus: eventBus,
-    linkService: pdfLinkService,
-    findController: pdfFindController,
+const pdfViewer = new pdfjsViewer.PDFViewer({
+  container: container,
+  eventBus: eventBus,
+  linkService: pdfLinkService,
+  findController: pdfFindController,
 });
 pdfLinkService.setViewer(pdfViewer);
 
 eventBus.on("pagesinit", function () {
-    // We can use pdfViewer now, e.g. let's change default scale.
-    pdfViewer.currentScaleValue = "page-width";
+  // We can use pdfViewer now, e.g. let's change default scale.
+  pdfViewer.currentScaleValue = "page-width";
 
-    if (SEARCH_FOR) {
-        pdfFindController.executeCommand("find", { query: SEARCH_FOR });
-    }
+  if (SEARCH_FOR) {
+    pdfFindController.executeCommand("find", { query: SEARCH_FOR });
+  }
 });
 
-function render() {
-
-}
+function render() {}
 
 function goBack() {
-    if (lastPosition) {
-        lastPosition.scrollIntoView()
-        lastPosition = null
-    }
+  if (lastPosition) {
+    lastPosition = null;
+    container.scroll(0, lastPositionOffset);
+  }
 }
 
 document.getElementById("back").addEventListener("click", goBack);
 
-document.addEventListener('click', function (event) {
-    if (event.target.tagName == 'A' && event.target.className == "internalLink") {
-        lastPosition = event.target
+document.addEventListener(
+  "click",
+  function (event) {
+    if (
+      event.target.tagName === "A" &&
+      event.target.className === "internalLink"
+    ) {
+      lastPosition = event.target;
     }
-}, false);
+  },
+  false
+);
+
+viewer.addEventListener(
+  "mousedown",
+  function (event) {
+    lastPositionOffset = -event.currentTarget.getBoundingClientRect().top + 32;
+  },
+  false
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
-var webpack = require('webpack');
-var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: [
-        'webpack/hot/dev-server',
-        './src/index'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+  entry: ["webpack/hot/dev-server", "./src/index"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
         ],
-    },
-}
+      },
+    ],
+  },
+};
